f96c5d1b034ee6add3848ea18afcc1b5c1ac4c1a
Lorenzo <lorenzo.osimani@studio.unibo.it>
2022-03-14 11:55:59 +0100

    Exercise 1 and 2

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -1,5 +1,7 @@
 package u03
 
+import u02.Optionals.Option
+
 object Lists extends App:
 
   // A generic linkedlist
@@ -8,6 +10,7 @@ object Lists extends App:
     case Nil()
   // a companion object (i.e., module) for List
   object List:
+    import Option.*
 
     def sum(l: List[Int]): Int = l match
       case Cons(h, t) => h + sum(t)
@@ -21,6 +24,34 @@ object Lists extends App:
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t) => filter(t)(pred)
       case Nil() => Nil()
+
+    def drop[A](l: List[A], n: Int): List[A] = l match
+      case Cons(h, t) if n > 0 => drop(t, n - 1)
+      case list => list
+
+    def append[A](left: List[A], right: List[A]): List[A] = left match
+      case Cons(h, t) => Cons(h, append(t, right))
+      case Nil() => right
+
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+      case Nil() => Nil()
+
+    def map2[A, B](l: List[A])(mapper: A => B): List[B] =
+      flatMap(l)(v => Cons(mapper(v), Nil()))
+
+    def filter2[A](l: List[A])(pred: A => Boolean): List[A] =
+      flatMap(l)(v => if pred(v) then Cons(v, Nil()) else Nil())
+
+    def max(l: List[Int]): Option[Int] =
+      def maxIteration(l: List[Int], currentMax: Option[Int]): Option[Int] = (l, currentMax) match
+        case (Cons(h, t), Some(x)) if (h <= x) => maxIteration(t, Some(x))
+        case (Cons(h, t), _) => maxIteration(t, Some(h))
+        case (Nil(), current) => current
+
+      maxIteration(l, None())
+
+
 
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -3,9 +3,11 @@ package u03
 import org.junit.*
 import org.junit.Assert.*
 import Lists.*
+import u02.Optionals.*
 
 class ListTest:
   import List.*
+  import Option.*
 
   val l: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
 
@@ -20,3 +22,22 @@ class ListTest:
   @Test def testFilter() =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
+
+  @Test def TestDrop() =
+    assertEquals(Cons(20, Cons(30, Nil())), drop (l , 1)) // Cons (20 , Cons (30 , Nil ()))
+    assertEquals(Cons(30, Nil()), drop(l , 2)) // Cons (30 , Nil ())
+    assertEquals(Nil(), drop (l , 5))
+
+  @Test def TestAppend() =
+    val tail = Cons (40 , Nil ())
+    assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(l , tail ))
+
+  @Test def testFlatMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))),
+      List.flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))),
+      List.flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+
+  @Test def testMax() =
+    assertEquals(Some(25), max(Cons(10, Cons(25, Cons(20, Nil())))))
+    assertEquals(None(), max(Nil()))
